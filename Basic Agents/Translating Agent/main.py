bfddb052463427168cdf9a797040346ce156b44e
Asiya Khan <160250124+asiyakhan990@users.noreply.github.com>
2025-06-20 00:10:28 +0500

    first Commit

--- Basic Agents/Translating Agent/main.py
+++ Basic Agents/Translating Agent/main.py
@@ -2,15 +2,10 @@ from dotenv import load_dotenv
 import os
 from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, RunConfig
 
-
 load_dotenv()
+
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
-# Check if the API key is present; if not, raise an error
-if not gemini_api_key:
-    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
-
-#Reference: https://ai.google.dev/gemini-api/docs/openai
 external_client = AsyncOpenAI(
     api_key=gemini_api_key,
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
